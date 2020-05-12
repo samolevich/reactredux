0d99ad3d066df00bea007bf6789edbb862627e38
samolevich <samolevich@gmail.com>
2020-05-12 19:35:18 +0300

    add/sub counter by redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-
 class App extends Component {
   render() {
-    return <div>{this.props.counter}</div>;
+    return (
+      <>
+        <div>{this.props.counter}</div>
+        <button onClick={this.props.addCounter}>+</button>
+        <button onClick={this.props.subCounter}>-</button>
+      </>
+    );
   }
 }
+
+export const ADD = 'ADD';
+export const SUB = 'SUB';
+
+const addCounter = () => ({type: ADD});
+
+const subCounter = () => ({type: SUB});
 
 const mapStateToProps = state => {
   return {
-    counter: state.counter
-  }
-}
+    counter: state.counter,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-  
-}
+  return {
+    addCounter: () => dispatch(addCounter()),
+    subCounter: () => dispatch(subCounter()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { ADD, SUB } from './App';
 
 const initialState = { counter: 110 };
 
-const reducer = (state = initialState, action) => {
-  return state;
-};
+const reducer = (state = initialState, { type }) => {
+  switch (type) {
+
+  case ADD:
+    return {counter: state.counter + 1}
+
+  case SUB:
+    return {counter: state.counter - 1}
+
+  default:
+    return state
+  }
+}
+
 
 const store = createStore(reducer);
 
